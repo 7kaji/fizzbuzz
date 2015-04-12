a721deb648a78a520c4289341edcddbb88d16501
7kaji <hirotonbuu@gmail.com>
2015-04-12 17:44:09 +0900

    updated

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,24 @@
 
 # FizzBuzz
 class FizzBuzz
+  def say(n)
+    return 'FizzBuzz' if fizzbuzz? n
+    return 'Fizz' if fizz? n
+    return 'Buzz' if buzz? n
+    n
+  end
 
+  private
+
+  def fizzbuzz?(n)
+    fizz?(n) && buzz?(n)
+  end
+
+  def fizz?(n)
+    n % 3 == 0
+  end
+
+  def buzz?(n)
+    n % 5 == 0
+  end
 end
