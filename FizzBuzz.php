33571fbc54393b274c6bb106541fa798b94d9cb7
7kaji <hirotonbuu@gmail.com>
2015-12-17 02:34:58 +0900

    Add PHP ver.

---
+++ FizzBuzz.php
@@ -0,0 +1,44 @@
+#!/usr/bin/env php
+
+<?php
+class FizzBuzz
+{
+    function say($n)
+    {
+        if($this->fizz_buzz($n)){
+            return "FizzBuzz";
+        }
+        elseif($this->fizz($n)){
+            return "Fizz";
+        }
+        elseif($this->buzz($n)){
+            return "Buzz";
+        }
+        else{
+            return $n;
+        }
+    }
+
+    private function fizz_buzz($n)
+    {
+        if($this->fizz($n) && $this->buzz($n)){ return true;}else{ return false;}
+    }
+
+    private function fizz($n)
+    {
+        if($n % 3 == 0){ return true;}else{ return false;}
+        ($n % 3 == 0) ? true : false;
+    }
+
+    private function buzz($n)
+    {
+        if($n % 5 == 0){ return true;}else{ return false;}
+    }
+
+}
+
+// $numbers = new FizzBuzz();
+// for ($i = 1; $i <= 30; $i++)
+// {
+//     echo("{$i}\t:{$numbers->say($i)}\n");
+// }
