954334c66186fc796308da7fa74b034b0c1439cf
7kaji <hirotonbuu@gmail.com>
2015-12-17 23:47:09 +0900

    fix indent

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,10 +1,8 @@
 #!/usr/bin/env php
 
 <?php
-class FizzBuzz
-{
-    function say($n)
-    {
+class FizzBuzz{
+    function say($n){
         if($this->fizz_buzz($n)){
             return "FizzBuzz";
         }
@@ -19,26 +17,22 @@ class FizzBuzz
         }
     }
 
-    private function fizz_buzz($n)
-    {
+    private function fizz_buzz($n){
         if($this->fizz($n) && $this->buzz($n)){ return true;}else{ return false;}
     }
 
-    private function fizz($n)
-    {
+    private function fizz($n){
         if($n % 3 == 0){ return true;}else{ return false;}
-        ($n % 3 == 0) ? true : false;
     }
 
-    private function buzz($n)
-    {
+    private function buzz($n){
         if($n % 5 == 0){ return true;}else{ return false;}
     }
 
 }
 
-// $numbers = new FizzBuzz();
-// for ($i = 1; $i <= 30; $i++)
-// {
-//     echo("{$i}\t:{$numbers->say($i)}\n");
-// }
+$numbers = new FizzBuzz();
+for ($i = 1; $i <= 30; $i++)
+{
+    echo("{$i}\t:{$numbers->say($i)}\n");
+}
